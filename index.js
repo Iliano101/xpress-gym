6d88cefb4a7a1c4d6da943ce9f36ce2f4379fe33
Ilian Delagrange <iliandelagrange@gmail.com>
2023-10-31 11:08:26 -0400

    Removed hard coded values

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 $(document).ready(function () {
+    autofill();
     fetchValidators();
 });
+
+function autofill() {
+    const params = new URL(window.location.href).searchParams
+    console.log(params);
+
+    params.forEach((x, y) => {
+        if (y === "Gender") {
+            if (x === "M") {
+
+                document.getElementById("GenderM").checked = true;
+                document.getElementById("GenderF").checked = false;
+            }
+            else {
+                document.getElementById("GenderF").checked = true;
+                document.getElementById("GenderM").checked = false;
+
+            }
+        }
+        else {
+            document.getElementById(y).value = x
+        }
+
+    });
+
+}
+
 
 async function fetchValidators() {
     const url = 'https://gym-form-proxy.onrender.com';
