9a75785843bb57f20fbf1fc6d00407bdb19afe53
Ilian Delagrange <iliandelagrange@gmail.com>
2024-02-06 13:42:57 -0500

    Really updated the url

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const STORED_VALIDATORS_NAMES = {
     date: "validatorsDate",
 };
 
-const PROXY_URL = "https://xpress-gym-agent.vercel.app/gym";
+const PROXY_URL = "https://xpress-gym-agent.vercel.app";
 const GITHUB_API_URL =
     "https://api.github.com/repos/iliano101/xpress-gym/commits/vercel";
 const CURRENT_VERSION_STORAGE_KEY = "currentVersion";
